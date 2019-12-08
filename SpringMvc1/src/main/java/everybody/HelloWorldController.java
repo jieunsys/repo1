66ed8c01ad3c50367b4d6ff568fc2628217f0e32
delphy <delphy@DESKTOP-JM5CHQB>
2019-12-08 16:42:40 +0900

    fix scan bug

--- SpringMvc1/src/main/java/everybody/HelloWorldController.java
+++ SpringMvc1/src/main/java/everybody/HelloWorldController.java
@@ -3,15 +3,18 @@ package everybody;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
 
+@Controller
 public class HelloWorldController extends AbstractController {
 
-	@Override
-	protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response)
-			throws Exception {
-
+	@RequestMapping(value="/test1.do", method=RequestMethod.GET)
+	protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response) throws Exception {
+System.out.println("################ start");
 		ModelAndView model = new ModelAndView("HelloWorldPage");
 		model.addObject("msg", "helloworld 한글테스트에요1234");
 
